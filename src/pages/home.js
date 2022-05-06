21f801756855715fb234b06edc88dba848c5bd73
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-05-06 13:12:07 -0300

    feat: Header and Feature

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import { JumbotronContainer } from '../containers/jumbotron'
-import { FooterContainer } from '../containers/footer';
-import { FaqsContainer } from '../containers/faqs';
+import React from "react";
+import { Feature, OptForm } from "../components";
+import { HeaderContainer } from "../containers/header";
+import { JumbotronContainer } from "../containers/jumbotron";
+import { FooterContainer } from "../containers/footer";
+import { FaqsContainer } from "../containers/faqs";
 
 export default function Home() {
-    return (
-        <React.Fragment>
-            <JumbotronContainer/>
-            <FaqsContainer />
-            <FooterContainer />
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited films, TV prgrammes and more.</Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </React.Fragment>
+  );
 }

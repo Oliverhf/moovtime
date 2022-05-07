d2ad9808278173ca25317f9b418102c9622c68c8
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-05-07 19:37:12 -0300

    feat: use-auth-listener custom hook and Browse page with use-content custom hook

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
-import React from 'react';
-import {BrowserRouter as Router, Switch} from 'react-router-dom';
-import { Home, Browse, Signin, Signup } from './pages';
-import * as ROUTES from './constants/routes';
-import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
-
+import React from "react";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { Home, Browse, Signin, Signup } from "./pages";
+import * as ROUTES from "./constants/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
 
 export default function App() {
-  const user = null
-  return  (
+  const { user } = useAuthListener();
+  console.log(user);
+
+  return (
     <Router>
       <Switch>
-      <IsUserRedirect 
-          user={user} 
-          loggedInPath={ROUTES.BROWSE} 
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
           path={ROUTES.SIGN_IN}
-          
         >
           <Signin />
         </IsUserRedirect>
-        <IsUserRedirect 
-            user={user} 
-            loggedInPath={ROUTES.BROWSE} 
-            path={ROUTES.SIGN_UP}
-            
-          >
-            <Signup />
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.SIGN_UP}
+        >
+          <Signup />
         </IsUserRedirect>
-      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
           <Browse />
-      </ProtectedRoute>
-      <IsUserRedirect 
-            user={user} 
-            loggedInPath={ROUTES.BROWSE} 
-            path={ROUTES.HOME}
-            exact
-          >
+        </ProtectedRoute>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+          exact
+        >
           <Home />
-      </IsUserRedirect>
+        </IsUserRedirect>
       </Switch>
-        
     </Router>
-
-  ) 
+  );
 }
-
-
-
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,3 +1,14 @@
-export default function Signup() {
+
+import { useContent } from "../hooks"
+
+export default function Browse() {
+
+    // we nee the series and the films
+    const { series } = useContent('series')
+    const { films } = useContent('films')
+    console.table(series)
+    console.table(films)
+    // we need slides
+    // pass it to the browse container
     return <p>Hello form the Browse!</p>
 }

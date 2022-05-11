10a4e7912066f26e467d2941c9b10191ea36d0f6
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-05-10 23:51:44 -0300

    feat: Feature, Card components and props

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -39,7 +39,11 @@ Card.Group = function CardGroup({children, ...restProps}) {
 }
 
 Card.SubTitle = function CardSubTitle({children, ...restProps}) {
-    return <Group {...restProps}>{children}</Group>
+    return <SubTitle {...restProps}>{children}</SubTitle>
+}
+
+Card.Title = function CardTitle({children, ...restProps}) {
+    return <Title {...restProps}>{children}</Title>
 }
 
 Card.Text = function CardText({children , ...restProps}) {
@@ -48,6 +52,35 @@ Card.Text = function CardText({children , ...restProps}) {
 
 Card.Meta = function CardMeta({children , ...restProps}) {
     return <Meta {...restProps}>{children}</Meta>
+}
+
+Card.Feature = function CardFeature({children, category, ...restProps}) {
+    const { showFeature, itemFeature, setShowFeature} = useContext(FeatureContext)
+    return showFeature ? (
+        <Feature 
+        {...restProps}
+        src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}>
+            <Content>
+                <FeatureTitle>{itemFeature.title}</FeatureTitle>
+                <FeatureText>{itemFeature.description}</FeatureText>
+                <FeatureClose 
+                    onClick={() => setShowFeature(false)}>
+                        <img src='/images/icons/close.png' alt="close" />
+                </FeatureClose>
+         
+
+            <Group margin="30px 0" flexDirection="row" alignItems="center">
+                <Maturity rating={itemFeature.maturity}>{itemFeature.maturity < 12 ? 'PG' : itemFeature.maturity}</Maturity>
+                <FeatureText fontWeight="bold">
+                    {itemFeature.genre.charAt(0).toUpperCase() + itemFeature.genre.slice(1)}
+                </FeatureText>
+            </Group>
+            {children}
+            </Content>
+        </Feature>
+    ) : null
+
+
 }
 
 
